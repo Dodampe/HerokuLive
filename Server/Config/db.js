3b4fa109e02d671d0d7c593389627a5be3bab1e6
Dodampe <wijesiri2001@gmail.com>
2022-04-15 09:16:43 -0400

    ICE 13 live

--- Server/Config/db.js
+++ Server/Config/db.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.SessionSecret = exports.HostName = exports.RemoteURI = exports.LocalURI = void 0;
 exports.LocalURI = "mongodb://127.0.0.1/contacts";
-exports.RemoteURI = "mongodb+srv://devin:Naruto2020@cluster0.xwok9.mongodb.net/Users?retryWrites=true&w=majority";
-exports.HostName = "Mongo DB Atlas";
+exports.RemoteURI = "mongodb+srv://thomas:k64TLy7vaCgsKWpb@cluster0.askzb.mongodb.net/contacts?retryWrites=true&w=majority";
+exports.HostName = "MongoDB Atlas";
 exports.SessionSecret = "WEBD6201SessionSecret";
 //# sourceMappingURL=db.js.map
--- Server/Config/db.ts
+++ Server/Config/db.ts
@@ -1,5 +1,5 @@
 export const LocalURI = "mongodb://127.0.0.1/contacts";
 export const RemoteURI = "mongodb+srv://devin:Naruto2020@cluster0.xwok9.mongodb.net/Users?retryWrites=true&w=majority"
-export const HostName = "Mongo DB Atlas";
-//export const HostName = "MongoDB Atlas";
+//export const HostName = "localhost";
+export const HostName = "MongoDB Atlas";
 export const SessionSecret = "WEBD6201SessionSecret";
